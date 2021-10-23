643ad2ec729ccd3be7124a2d51d813043c303fff
Showrin <showrinece15@gmail.com>
2021-10-23 22:15:32 +0600

    Read secret keys from env file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCqPVCmrz5WYfdBcGjlP5ouIYEF2KBMD0",
-  authDomain: "locally-stage.firebaseapp.com",
-  projectId: "locally-stage",
-  storageBucket: "locally-stage.appspot.com",
-  messagingSenderId: "147063185191",
-  appId: "1:147063185191:web:bdb7de725c221b87ff1302",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
